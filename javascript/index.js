31f5b87da319bdffa20c88d109cf23fd7ce78118
a1p4ca <dev.alpaca@gmail.com>
2019-04-17 10:27:49 +0900

    Add javascript implementation

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+function evil (target) {
+  return target.replace('\u003B', '\u037E');
+}
+
+function helloWorld () {
+  const semi = 'Semicolon';
+  console.log(`Hello, ${semi}!`);
+}
+
+console.log(evil(helloWorld.toString()))
